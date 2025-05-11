23f248ca519b0e2229119196bc935c4548a9d39e
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-11 12:25:03 +0600

    SignUp Functionality or Create a user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,57 @@
-export const signup = (req, res) => {
-  res.send("Signup route");
-}
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+
+  try {
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "All fields must be required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email: email,
+      fullName: fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        fullName: newUser.fullName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "User not created" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const login = (req, res) => {
   res.send("login route");
-}
+};
 
 export const logout = (req, res) => {
   res.send("logout route");
-}
+};
