25bd95ab07e3c345d3ae899df29985bd055f91c5
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-11 20:50:37 +0600

    update profile & middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup,updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-
-router.get("/login", login);
-
+router.post("/login", login);
 router.post("/logout", logout);
 
-export default router;
+router.put("/update-profile", protectRoute, updateProfile);
 
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
