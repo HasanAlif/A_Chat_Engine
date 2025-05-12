a200739ba049951b109dfd98f6103e0921d4eaee
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-12 19:47:01 +0600

    Update Profile & ProfilePage

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,8 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const app = express();
 
+
+app.use(express.json({ limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
