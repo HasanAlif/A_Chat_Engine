9c9b4f7aa824a24ea8a2e1b705acc97dc66d17d9
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-15 23:29:53 +0600

    Implementing ChatStore with Socket.io

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
 
-export const useChatStore = create((set,get) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -32,11 +33,13 @@ export const useChatStore = create((set,get) => ({
       set({ isMessagesLoading: false });
     }
   },
-
   sendMessage: async (messageData) => {
     const { selectedUser, messages } = get();
     try {
-      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
       set({ messages: [...messages, res.data] });
     } catch (error) {
       toast.error(error.response.data.message);
@@ -44,13 +47,26 @@ export const useChatStore = create((set,get) => ({
   },
 
   subscribeToMessages: () => {
-    
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
   },
 
   unsubscribeFromMessages: () => {
-   
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
   },
-
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
